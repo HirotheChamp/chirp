f01f84fd05a5eb310d666c7703277f5b436a249d
Kyle Hiroshi Young <93508000+HirotheChamp@users.noreply.github.com>
2023-05-09 09:28:34 -0700

    sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SignIn, SignInButton } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -15,6 +16,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+<div>
+  <h1>sign in</h1>
+  <SignInButton/>
+</div>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
